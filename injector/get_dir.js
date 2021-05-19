e491dd5cede4b0a514629eaae05dc5e0386948c7
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-05-19 16:30:45 -0400

    parse with readable backslashes

--- injector/get_dir.js
+++ injector/get_dir.js
@@ -1,3 +1,7 @@
-const platform = require('platform-folders')
-const discord_loc = `${}`
-console.log(platform.getDataHome());
+const platform = require('platform-folders');
+const fs = require('fs');
+const discord_loc = `${platform.getDataHome()}\\discord`;
+const result = discord_loc.split("\\").join("\\\\");
+fs.mkdir("./info", ()=>{})
+fs.writeFileSync('./info/file.json', `{\n "discord_location":"${result}" \n}`, ()=>{
+});
